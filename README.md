5f4baea93f8a0ae77bbf0e552ea519b5cf4e1bba
Adam Gregory <dostoy320@gmail.com>
2013-10-22 21:22:37 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-Working My Way Through Test-Driven Web Development with Python
---------------------------------------------------------------
+
+Working Through
+===============
+*Test-Driven Web Development with Python*
+========================================
 
 Using the [online version][tddp] of the book.
 
+#### Day 1:
+* Python3, PIP-1.4.1, and Selenium 2.35.0 installed.
+  (already had Django 1.5.4 installed)
+* created the first test: *functional_tests.py*
+* created a Django project called **superlists**
+* initialized the Test-Driven-Django repository
+* learned a bit of Markdown from [Daring Fireball][df]
 
-[tddp]: http://chimera.labs.oreilly.com/books/1234000000754
-"Test-Driven Main Page"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[tddp]: http://chimera.labs.oreilly.com/books/1234000000754 "Test Driven Site"
+[df]: http://daringfireball.net/projects/markdown/syntax
--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Jeni visits the following url:
+		self.browser.get('http://localhost:8000')
+
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+		# She is invited to enter a to-do item straight away
+		[...rest of comments as before]
+
+		# She types "Buy fancy cheeses" into a text box.
+
+		# When she hits enter, the page updates and now lists:
+		# "1. Buy fancy cheeses" as an item in the to-do list.
+
+		# There is still a text box inviting her to enter another item.
+		# Jeni adds "buy a case of wine" to the list.
+
+		# The page updates again and now shows both items on her list.
+
+ 		# The site has generated a unique url, with text explaining its existence.
+
+		# She visits the url, and her list is there.
+
+		# Satisfied, she goes to do something else.
+
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
+
